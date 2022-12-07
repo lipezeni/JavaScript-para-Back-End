731a927044085f37f994389e5e97f3fabf145b6e
Felipe Zeni <felipezeni06@gmail.com>
2022-12-07 02:08:47 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+// boolean
+// true OU false
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
