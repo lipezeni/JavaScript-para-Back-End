02c665b01720233c520bf10f5ea79c7e2da279eb
Felipe Zeni <felipezeni06@gmail.com>
2022-12-08 00:24:28 -0300

    Codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// consversão implicita
+const numero1 = 456;
+const numeroString = Number("456");
+
+// Number()
+// String()
+console.log(numero1 + numeroString);
+
+// conversão explicita
