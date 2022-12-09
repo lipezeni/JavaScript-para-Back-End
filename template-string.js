c7640b1d9074a6c3d12b1b65337236a1834816cd
Felipe Zeni <felipezeni06@gmail.com>
2022-12-08 22:08:30 -0300

    Comparações, template-string e operador ternário

---
+++ template-string.js
@@ -0,0 +1,9 @@
+const nome = "Felipe";
+const idade = 2022-2007;
+const cidadeDeNascimento = "Porto Alegre";
+
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// if (idadeCliente >= idadeCliente) {
+//     console.log("Cerveja");
+// } else {
+//     console.log("Suco");
+// }
+
+            //condição                    //true      //false
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco");
