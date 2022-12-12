b2037ea592c3b3393d150039352083808361c91f
lipezeni <felipezeni06@gmail.com>
2022-12-12 18:06:22 -0300

    Objetos

---
+++ Objetos/aula1/alterar.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: "Luma",
+    profissão: "Engenharia",
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 222333444";
+pessoa.nome = "Luma Silva";
+console.log(pessoa);
---
+++ Objetos/aula1/objeto.js
@@ -0,0 +1,7 @@
+const listaCPFs = [111111, 22222, 33333];
+const informacoesPessoa = ["nome", "José", "idade", 32, "CPF", "111222333"];
+
+const objetopessoa = {
+    nome: "José",
+    idade: 32
+}
